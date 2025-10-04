f01b1c0d7d71413a1f076072a66ff18a017714df
Bernie Telles <btelles@gmail.com>
2025-10-03 20:24:55 -0700

    [btelles] add command to teleop koch1.1 on personal desktop.

--- teleop.sh
+++ teleop.sh
@@ -5,4 +5,12 @@
 sudo chmod 666 /dev/ttyACM0
 sudo chmod 666 /dev/ttyACM1
 
-python3 teleop.py
+python -m lerobot.teleoperate \
+    --robot.type=koch_follower \
+    --robot.port=/dev/ttyACM1 \
+    --robot.id=white \
+    --teleop.type=koch_leader \
+    --teleop.port=/dev/ttyACM0 \
+    --teleop.id=red \
+    --display_data=true
+    #--robot.cameras="{ front: {type: opencv, index_or_path: 0, width: 1920, height: 1080, fps: 30}}" \
